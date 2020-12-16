b8ba9e834158b4c743f7ad2333af27ef1c10df7d
yg kim <54833991+yg-kim-korean@users.noreply.github.com>
2020-12-17 01:43:34 +0900

    Fin Todolist

--- react_exercise_make-todolist/src/components/TodoCreate.js
+++ react_exercise_make-todolist/src/components/TodoCreate.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import styled,{ css } from 'styled-components'
 import {MdAdd} from 'react-icons/md'
+import { useTodoDispatch, useTodoNextId } from '../TodoContext';
 
 const CircleButton = styled.div`
     background: #38d9a9;
@@ -76,14 +77,32 @@ const Input = styled.input`
 
 export default function TodoCreate() {
     const [open, setOpen] = useState(false);
+    const [value, setValue] = useState('');
 
+    const dispatch = useTodoDispatch();
+    const nextId = useTodoNextId();
     const onToggle = () => setOpen(!open);
+    const onChange = e => setValue(e.target.value);
+    const onSubmit = e => {
+      e.preventDefault();
+      dispatch({
+        type:'CREATE',
+        todo:{
+          id: nextId.current,
+          text:value,
+          done:false
+        }
+      });
+      setValue('');
+      setOpen(false);
+      nextId.current+=1;
+    }
     return (
         <>
           {open && (
               <InsertFormPositioner>
-                  <InsertForm>
-                      <Input autoFocus placeholder='할일을 입력한 후 Enter를 누르세요' />
+                  <InsertForm onSubmit={onSubmit}>
+                      <Input autoFocus placeholder='할일을 입력한 후 Enter를 누르세요' onChange={onChange}/>
                   </InsertForm>
               </InsertFormPositioner>
           )}  
