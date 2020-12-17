dd15608f9a722a97a3b63f9cccf018870b62be57
yg kim <54833991+yg-kim-korean@users.noreply.github.com>
2020-12-17 09:57:17 +0900

    useState대신 useReducer로 변경하기
    useReducer 로 구현했을 때의 장점은 useState 의 setState 함수를 여러번 사용하지 않아도 된다는점과, 리듀서로 로직을 분리했으니 다른곳에서도 쉽게 재사용을 할 수 있다는 점

--- react_api-integrate-basic/src/Users.js
+++ react_api-integrate-basic/src/Users.js
@@ -1,37 +1,83 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import axios from 'axios';
+
+function reducer(state,action){
+    switch(action.type){
+        case 'LOADING':
+            return {
+                loading:true,
+                data:null,
+                error:null
+            };
+        case 'SUCCESS':
+            return {
+                loading:false,
+                data:action.data,
+                error:null
+            };
+        case 'ERROR':
+            return {
+                loading:false,
+                data:null,
+                error:action.error
+            };
+        default:
+            throw new Error (`Unhandled action type: ${action.type}`);
+    }
+}
+
 export default function Users() {
-    const [users, setUsers] = useState(null);
-    const [loading, setLoading ] = useState(false);
-    const [error, setError] = useState(null);
-    const fetchUsers = async () =>{ //useEffect 에 첫번째 파라미터로 등록하는 함수에는 async 를 사용 할 수 없기 때문에 함수 내부에서 async 를 사용하는 새로운 함수를 선언해주어야 합니다.
+
+    const [state,dispatch] = useReducer(reducer,{
+        loading:false,
+        data:null,
+        error:null
+    })
+    // const [users, setUsers] = useState(null);
+    // const [loading, setLoading ] = useState(false);
+    // const [error, setError] = useState(null);
+    // const fetchUsers = async () =>{ //useEffect 에 첫번째 파라미터로 등록하는 함수에는 async 를 사용 할 수 없기 때문에 함수 내부에서 async 를 사용하는 새로운 함수를 선언해주어야 합니다.
+    //     try{
+    //         //요청이 시작될때는 error와 users를 초기화하고
+    //         setError(null);
+    //         setUsers(null);
+    //         //loading 상태를 true로 바꿈
+    //         setLoading(true);
+    //         const response = await axios.get(
+    //             'https://jsonplaceholder.typicode.com/users'
+    //         );
+    //         setUsers(response.data); // 받아온데이터 user로 넣기
+    //     }
+    //     catch(e){
+    //         setError(e)
+    //     }
+    //     setLoading(false);
+    // };
+    const fetchUsers = async () =>{
+        dispatch({type:'LOADING'});
         try{
-            //요청이 시작될때는 error와 users를 초기화하고
-            setError(null);
-            setUsers(null);
-            //loading 상태를 true로 바꿈
-            setLoading(true);
             const response = await axios.get(
                 'https://jsonplaceholder.typicode.com/users'
             );
-            setUsers(response.data); // 받아온데이터 user로 넣기
+            dispatch({type:'SUCCESS', data: response.data});
         }
         catch(e){
-            setError(e)
+            dispatch({type:'ERROR', error:e})
         }
-        setLoading(false);
-    };
+    }
     useEffect(()=>{
         fetchUsers();
     },[]);
 
+    const {loading, data, error} = state;
+
     if (loading) return <div>로딩중...</div>
     if (error) return <div>에러가 발생했습니다.</div>
-    if (!users) return null;
+    if (!data) return null;
     return (
         <>
             <ul>
-                {users.map(user=>(
+                {data.map(user=>(
                     <li key={user.id}>
                         {user.username} ({user.name})
                     </li>
