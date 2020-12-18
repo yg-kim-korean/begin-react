5307b444995d20083a35722b02d4fe9874a032c3
yg kim <54833991+yg-kim-korean@users.noreply.github.com>
2020-12-19 00:12:01 +0900

    json-server
    프런트엔드 개발 연습을 하게 될 때마다 연습을 하는 것 분인데 직접 백엔드 개발을 하는것은 조금 귀찮습니다. 특히나 아직 백엔드쪽 지식이 없다면 더욱 힘들죠.
    
    우리는 연습을 위해서 프런트엔드 프로젝트에서 실무와 비슷한 느낌으로 하기 위하여 가짜 API 서버를 만들어볼건데요, 이 때 사용되는 도구가 json-server 입니다. 이 도구를 사용하면 json 파일 하나만 있으면 연습용 서버를 쉽게 구성 할 수 있습니다.
    
    참고로 이 도구는 프로덕션에서 사용하기위하여 만들어진것이 아니기 때문에 이 서버를 사용하여 실제 프로젝트를 개발 하시면 안됩니다. 실제 프로젝트 개발 할 때에는 백엔드쪽 공부를 하셔서 서버를 직접 준비하시거나 Firebase 를 사용해서 구현을 하셔야합니다.
    data.json 파일 생성하고
    $ npx json-server ./data.json --port 4000

--- react_learn-redux-middleware/src/api/posts.js
+++ react_learn-redux-middleware/src/api/posts.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 //n밀리세컨드 동안 기다리는 프로미스를 만들어주는 함수
 const sleep = n => new Promise(resolve => setTimeout(resolve,n));
 
@@ -23,11 +24,15 @@ const posts = [
 
 //포스트 목록을 가져오는 비동기 함수
 export const getPosts = async () =>{
-    await sleep(500); //0.5초 쉬고
-    return posts; //posts 배열
+    // await sleep(500); //0.5초 쉬고
+    // return posts; //posts 배열
+    const response = await axios.get('http://localhost:4000/posts');
+    return response.data;
 }
 //ID로 포스트를 조회하는 비동기 함수
 export const getPostById = async (id) => {
-    await sleep(500);
-    return posts.find(post => post.id === id);
+    // await sleep(500);
+    // return posts.find(post => post.id === id);
+    const response = await axios.get(`http://localhost:4000/posts/${id}`);
+    return response.data;
 }
